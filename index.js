f5ab86caeb1c6acb23b20827e4d44254f0541171
Zane Werle <zanewerle@gmail.com>
2023-05-09 12:14:22 -0500

    added in the questions for the prompt, also imported code from my mini project just to have a reference when writing out the code (will delete from the repository later

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What would you like your logo to say? (Up to 3 characters)",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What color would you like the text to be? (Can be a color or a hexadecimal code)",
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape you would like in your logo!",
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+      type: "input",
+      name: "shapecolor",
+      message: "What color would you like your shape to be? (Can be a color or a hexadecimal code)",
+    }
+  ];
