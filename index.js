6e3c4fd69a876beddee4d35a043daef737c0c2f8
Zane Werle <zanewerle@gmail.com>
2023-05-09 12:45:16 -0500

    added in shapeColor to the paramaters inside of the shapes classes as well as added in a blank js file that the svg will be built into and then exported.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const questions = [
+const logoQuestions = [
     {
-      type: "input",
-      name: "title",
-      message: "What would you like your logo to say? (Up to 3 characters)",
+        type: "input",
+        name: "title",
+        message: "What would you like your logo to say? (Up to 3 characters)",
     },
     {
-      type: "input",
-      name: "textColor",
-      message: "What color would you like the text to be? (Can be a color or a hexadecimal code)",
+        type: "input",
+        name: "textColor",
+        message: "What color would you like the text to be? (Can be a color or a hexadecimal code)",
     },
     {
         type: "list",
@@ -19,8 +19,15 @@ const questions = [
         choices: ["Circle", "Triangle", "Square"]
     },
     {
-      type: "input",
-      name: "shapecolor",
-      message: "What color would you like your shape to be? (Can be a color or a hexadecimal code)",
+        type: "input",
+        name: "shapecolor",
+        message: "What color would you like your shape to be? (Can be a color or a hexadecimal code)",
     }
-  ];
+];
+
+inquirer.prompt(logoQuestions).then((response) => {
+
+    fs.writeFile("test.svg", logoRequested, (err) =>
+        err ? console.log(err) : console.log("Generated logo.svg")
+    );
+});
